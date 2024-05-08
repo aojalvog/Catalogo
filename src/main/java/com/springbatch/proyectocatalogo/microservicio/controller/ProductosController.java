29dd526a207e599297ffa4792ed8b03b0184519e
aojalvog <alvaro.ojalvo@gmail.com>
2024-05-08 13:50:15 +0200

    docs(Documentación final del proyecto): :memo:

--- src/main/java/com/springbatch/proyectocatalogo/microservicio/controller/ProductosController.java
+++ src/main/java/com/springbatch/proyectocatalogo/microservicio/controller/ProductosController.java
@@ -9,12 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springbatch.proyectocatalogo.microservicio.model.Productos;
 import com.springbatch.proyectocatalogo.microservicio.service.ProductosServices;
 
+/**
+ * Este es el controller de Productos donde vamos a dejar los /endPoints
+ */
 @RestController
 public class ProductosController {
 
+	/**
+	 * Inyectamos la clase de productosServices
+	 */
 	@Autowired
 	ProductosServices productosServices;
 
+	/**
+	 * Creación del endpoint /getCatalogo
+	 * 
+	 * @return lista de todos los productos que hay en el catálogo
+	 */
 	@GetMapping("/getCatalogo")
 	public List<Productos> getCatalogo() {
 		return productosServices.catalogo();
--- src/main/java/com/springbatch/proyectocatalogo/microservicio/service/ProductosServices.java
+++ src/main/java/com/springbatch/proyectocatalogo/microservicio/service/ProductosServices.java
@@ -9,5 +9,11 @@ import com.springbatch.proyectocatalogo.microservicio.model.Productos;
 @Service
 public interface ProductosServices {
 
+	/**
+	 * Obtiene el catálogo de productos.
+	 *
+	 * @return Una lista de objetos {@link Productos} que representan el catálogo de
+	 *         productos.
+	 */
 	List<Productos> catalogo();
 }
